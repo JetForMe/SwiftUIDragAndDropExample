4c3dbca2ad5c902ef7c4f34406f3a233bd151a56
Rick M <rmann@latencyzero.com>
2023-12-24 00:48:23 -0800

    Initial Commit

---
+++ DragCrash/DragCrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragCrashApp.swift
+//  DragCrash
+//
+//  Created by Rick Mann on 2023-12-24.
+//
+
+import SwiftUI
+
+@main
+struct DragCrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
